6dda83b98cb4a6c13b4372f9fe824fb71b996d50
Yaseen Joba <46541853+yaseenjoba@users.noreply.github.com>
2022-06-10 10:58:40 +0300

    Time: 38 ms, Memory: 12.6 MB - LeetHub

---
+++ two-sum/two-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>mm;
+        int j =1;
+        for(auto i : nums){
+            mm[i] = j++;
+        }
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            if(mm.count(target - nums[i])){
+                if(i == mm[target - nums[i]] - 1)continue;
+               ans.push_back(i);
+               ans.push_back(mm[target - nums[i]] - 1);
+               return ans;
+            }
+        }
+        return ans;
+    }
+};
