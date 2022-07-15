aba00c358caa3cfbb11fa5de1333dd6afdddc732
Yaseen Joba <46541853+yaseenjoba@users.noreply.github.com>
2022-07-15 12:38:28 +0300

    Time: 19 ms (90.87%), Space: 23.2 MB (89.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
+    bool vis[51][51];
     int cnt = 0;
-    int vis[60][60];
-    vector<vector<int>>gr;
-    void dfs(int  i , int j){
-        if(i >= gr.size() || j >= gr[0].size() || i <0 || j <0 || gr[i][j] == 0)return;
-        if(vis[i][j])return;
+    void dfs(int i , int j , vector<vector<int>>& grid){
+        if(i < 0 || i>=grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0 || vis[i][j])return;
+        vis[i][j] = true;
         cnt++;
-        vis[i][j] = 1;
-        dfs(i + 1 , j);        
-        dfs(i , j + 1);
-        dfs(i - 1 , j);        
-        dfs(i , j - 1);
+        dfs(i + 1 , j , grid);        
+        dfs(i , j  + 1, grid);
+        dfs(i , j - 1 , grid);
+        dfs(i - 1 , j , grid);
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        gr = grid;
         int ans = 0;
-        for(int  i = 0 ; i < grid.size() ;i++){
-            for(int  j = 0 ;j < grid[0].size() ;j++){
-                if(grid[i][j] == 1 && !vis[i][j]){
-                    cnt= 0;
-                    dfs(i , j);
+        for(int i = 0 ; i< grid.size(); i++){
+            for(int j = 0 ;j <grid[0].size() ;j++){
+                if(!vis[i][j] && grid[i][j] == 1){
+                    cnt = 0;
+                    dfs(i  , j , grid);
                     ans = max(ans , cnt);
                 }
             }
         }
-        return  ans;
+        return ans;
     }
 };
