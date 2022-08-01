4f5412b9edcd0223338b8292aa8c78ae5f4cb0b3
Yaseen Joba <46541853+yaseenjoba@users.noreply.github.com>
2022-08-01 12:03:05 +0300

    Time: 4 ms (28.94%), Space: 6.4 MB (53.20%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dp[101][101];
+    vector<vector<int>>dp;
     int dfs(int i , int j, int n ,int m){
         if(i >=n || j>=m)return 0;
         if(i == n - 1 && j == m - 1)return 1;
@@ -8,7 +8,7 @@ public:
         return dp[i][j] = dfs(i + 1 , j , n  , m) + dfs(i , j  + 1 , n , m);
     }
     int uniquePaths(int m, int n) {
-        memset(dp , -1 , sizeof dp);
+        dp.assign(n , vector<int>(m , -1));
         return dfs(0 , 0 , n , m);
     }
 };
