6b63ef8df29a08479dc214036db576fb1c1cd418
Yaseen Joba <46541853+yaseenjoba@users.noreply.github.com>
2022-08-05 16:50:18 +0300

    Time: 12 ms (64.91%), Space: 7.3 MB (74.99%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) {
-        if(s1.size()  > s2.size())return false;
-        vector<int>f1(26 , 0);        
-        vector<int>f2(26 , 0);
-        for(auto i : s1)f1[i - 'a']++;
+    bool checkInclusion(string pattern, string str) {
+      if(pattern.size() > str.size())return false;
+      vector<int>target(26 , 0) , freq(26 , 0);
+      for(auto  i : pattern){
+        target[i - 'a']++;
+      }
 
-        for(int i = 0 ; i < s1.size() ;i++){
-            f2[s2[i] - 'a']++;
-        }
-        if(f1 == f2)return true;
-        for(int i = s1.size() ; i < s2.size() ;i++){
-            f2[s2[i] - 'a']++;
-            f2[s2[i - s1.size()] - 'a']--;
-            if(i)
-            if(f1 == f2)return true; 
-        }
-        return false;
+      int l = 0 , r = pattern.size() - 1;
+      for(int i = l ; i <= r ;i++){
+        freq[str[i] - 'a']++;
+      }
 
+      while(r + 1 < str.size()){
+        if(freq == target){
+          return true;
+        }
+        r++;
+        freq[str[r] - 'a']++;
+        freq[str[l] - 'a']--;
+        l++;
+      }
+      return freq == target;
     }
 };
