909a01d8fbf3eb12d01514f3282e03acf54218c8
Yaseen Joba <46541853+yaseenjoba@users.noreply.github.com>
2022-08-05 16:42:55 +0300

    Time: 327 ms (23.13%), Space: 75.2 MB (17.45%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.cpp
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    int totalFruit(vector<int>& fruits) {
-        map<int,int> m;
-        int ans = 0,j = 0;
-        for(int i=0;i<fruits.size();i++){
-            m[fruits[i]]++;
-            if(m.size()<=2){
-                int cnt = 0;
-                for(auto it:m){
-                    cnt += it.second;
+    int totalFruit(vector<int>& arr) {
+        map<int ,int>freq;/*O(1)*/
+        int l = 0  ,ans = 0;
+        for(int r = 0 ; r < arr.size(); r++){
+          freq[arr[r]]++;
+          if(freq.size() <= 2){
+              int cnt = 0;
+              for(auto i : freq){
+                  cnt+= i.second;
+              }
+              ans = max(ans , cnt);
+          }else{
+              while(freq.size() > 2){
+                if(freq[arr[l]] == 1){
+                    freq.erase(arr[l]);
+                }else {
+                    freq[arr[l]]--;
                 }
-                ans = max(ans,cnt);
-            }
-            else{
-                while(m.size()>2){
-                    if(m[fruits[j]]==1){
-                        m.erase(fruits[j]);
-                    }
-                    else{
-                        m[fruits[j]]--;
-                    }
-                    j++;
-                }
-            }
+                l++;
+              }
+          }
+       
         }
-        return ans;
+         return ans;
     }
 };
