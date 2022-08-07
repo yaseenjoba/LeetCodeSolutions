0c8b218b2e42928f72b39fc3bb45a8425b1e1fd1
Yaseen Joba <46541853+yaseenjoba@users.noreply.github.com>
2022-08-07 14:55:30 +0300

    Time: 3 ms (64.07%), Space: 8.3 MB (23.78%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        vector<int>v(3,  0);
-        for(auto i : nums){
-            v[i]++;
-        }
-        for(auto &i : nums){
-            if(v[0]) i = 0 , v[0]--;            
-            else if (v[1]) i = 1 , v[1]--;
-            else if(v[2]) i = 2 , v[2]--;
+    void sortColors(vector<int>& arr) {
+         int l = 0 , r = arr.size() - 1;
+        for(int i = 0 ; i <= r;){
+            if(arr[i] == 0){
+              swap(arr[i] , arr[l]);
+              l++;
+              i++;
+            }else if(arr[i] == 1){
+              i++;
+            }else if(arr[i] == 2){
+              swap(arr[i] , arr[r]);
+              r--;
+            }
         }
     }
 };
