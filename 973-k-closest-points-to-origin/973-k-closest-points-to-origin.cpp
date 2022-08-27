0020eeb4b9d25c06fffbbc26af98eda28711cd45
Yaseen Joba <46541853+yaseenjoba@users.noreply.github.com>
2022-08-27 16:35:09 +0300

    Time: 455 ms (39.35%), Space: 70.7 MB (45.60%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,18 +1,44 @@
+class Point {
+ public:
+  int x = 0;
+  int y = 0;
+
+  Point(int x, int y) {
+    this->x = x;
+    this->y = y;
+  }
+
+  int distFromOrigin() const {
+    // ignoring sqrt
+    return (x * x) + (y * y);
+  }
+
+  const bool operator<(const Point& p) { return p.distFromOrigin() > this->distFromOrigin(); }
+};
+
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int, pair<int, int>>>pq;
+       vector<Point>p;
         for(auto i : points){
-            pq.push({(i[0] * i[0]) + (i[1] *i[1] ), {i[0] , i[1]}});
+            p.push_back(Point(i[0] , i[1]));
         }
-        for(int i = k ; i< points.size();i++){
-            pq.pop();
+        vector<Point>maxHeap(p.begin() , p.begin() + k);
+        make_heap(maxHeap.begin(), maxHeap.end());
+        for(int i = k ; i < p.size() ; i++){
+            if(p[i].distFromOrigin() < maxHeap.front().distFromOrigin()){
+                pop_heap(maxHeap.begin() , maxHeap.end());
+                maxHeap.pop_back();
+                maxHeap.push_back(p[i]);
+                push_heap(maxHeap.begin() , maxHeap.end());
+            }
         }
-        vector<vector<int>>ans;
-        while(!pq.empty()){
-            ans.push_back({pq.top().second.first , pq.top().second.second});
-            pq.pop();
+        
+        vector<vector<int>> ans;;
+        for(auto i : maxHeap){
+            ans.push_back({i.x , i.y});
         }
         return ans;
     }
+        
 };
