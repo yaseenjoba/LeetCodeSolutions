8d9612432a2af6f55a46c1f0aa6a96336293c4a6
Yaseen Joba <46541853+yaseenjoba@users.noreply.github.com>
2022-09-05 15:45:23 +0300

    Time: 65 ms (5.44%), Space: 11.7 MB (86.69%) - LeetHub

---
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>>ans;
+        if(root == NULL){
+            return ans;
+        }
+        queue<Node*>q;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            vector<int>res;
+            while(n--){
+                Node* t = q.front();
+                q.pop();
+                res.push_back(t->val);
+                for(auto i : t->children){
+                    q.push(i);
+                }
+            }
+            ans.push_back(res);
+        }
+        return ans;;
+    }
+};
