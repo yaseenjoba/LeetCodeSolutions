af2e61791a2b1f051828fb18deae7f8111efa3f0
Yaseen Joba <46541853+yaseenjoba@users.noreply.github.com>
2022-10-03 09:24:47 +0300

    Time: 1219 ms (16.67%), Space: 105.5 MB (50.00%) - LeetHub

--- 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.cpp
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.cpp
@@ -4,11 +4,21 @@ typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics
 class Solution {
 public:
     long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int diff) {
+        vector<int>arr1(nums1.size());        
+        vector<int>arr2(nums1.size());
+        int n = nums1.size();
         ordered_multiset s;
+        for(int i = 0 ; i< nums2.size() ;i++){
+            arr1[i] = nums1[i] - nums2[i];
+            arr2[i] = arr1[i] + diff;
+            s.insert(arr1[i]);
+        }
         long long ans = 0;
-        for(int i = 0 ; i < nums2.size() ;i++){
-            ans+= s.order_of_key(nums1[i] - nums2[i] + 1);
-            s.insert(nums1[i] - nums2[i]  - diff);
+        reverse(arr2.begin() ,arr2.end());
+        for(auto i : arr2){
+            s.erase(s.find_by_order(s.order_of_key(i - diff)));
+            ans+=s.order_of_key(i + 1);
+
         }
         return ans;
 
