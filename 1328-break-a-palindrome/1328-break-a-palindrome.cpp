45eff57e69f0968661ab40884f1338f5c5971f82
Yaseen Joba <46541853+yaseenjoba@users.noreply.github.com>
2022-10-10 22:53:09 +0300

    Time: 3 ms (46.17%), Space: 6.2 MB (45.46%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+           size_t sz = palindrome.size(); 
+        if (sz == 1)
+            return "";
+        for (size_t i = 0; i < sz/2; i++) {
+            if (palindrome[i] - 'a' > 0) {
+                palindrome[i] = 'a';
+                return palindrome;
+            }
+        }
+        palindrome[sz-1] = 'b';
+        return palindrome;
+    }
+};
