e6d10bd855a2a9ab3b81450e80e951d9e9a3679d
Yaseen Joba <46541853+yaseenjoba@users.noreply.github.com>
2022-10-28 11:34:40 +0300

    Time: 53 ms (79.15%), Space: 24.2 MB (16.68%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
+    void countSort(string &str){
+        string ans = "";
+        vector<int>freq(26 , 0);
+        for(auto i : str)freq[i - 'a']++;
+        for(int i = 0 ;i < 26 ;i++){
+            ans+= string(freq[i] , i + 'a');
+        }
+        str = ans;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string , vector<string>>mm;
         for(auto i : strs){
             auto v = i;
-            sort(v.begin() , v.end());
+            countSort(v);
             mm[v].push_back(i);
         }
         vector<vector<string>>ans;
